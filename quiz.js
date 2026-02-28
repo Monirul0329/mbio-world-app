3b2b2ecd606f2d05aa46965edef9e19b17dbaf08
Monirul0329 <monirulgen45@gmail.com>
2026-02-28 14:17:05 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "Cell is the unit of?",
+    options: ["Life", "Atom", "Energy", "Matter"],
+    correct: 0
+  },
+  {
+    question: "DNA full form?",
+    options: [
+      "Deoxy Nucleic Acid",
+      "Deoxyribo Nucleic Acid",
+      "Dynamic Acid",
+      "None"
+    ],
+    correct: 1
+  }
+];
